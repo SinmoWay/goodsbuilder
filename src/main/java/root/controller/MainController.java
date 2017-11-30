2cb9db3d7d47d5fb0e9b52b4b31c8acaa6754801
Richard Vojtenko <richard.vojtenko@vistar.su>
2017-12-01 01:23:16 +0300

    Подправлен внешний вид главного окна

--- src/main/java/root/controller/MainController.java
+++ src/main/java/root/controller/MainController.java
@@ -1,6 +1,9 @@
 package root.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TreeView;
 import org.springframework.beans.factory.annotation.Autowired;
 import root.db.service.ProductService;
 
@@ -11,13 +14,42 @@ public class MainController extends AbstractController {
     @Autowired
     private ProductService productService;
 
+    @FXML
+    private TreeView<String> mainTree;
+    @FXML
+    private Label infoText;
+    @FXML
+    private Button addButton;
+    @FXML
+    private Button editButton;
+    @FXML
+    private Button removeButton;
+
+
     @Override
     @PostConstruct
     public void init() {
+        infoText.setText("Успешно загружено");
     }
 
     @FXML
     public void unloadJson() {
+    }
+
+    @FXML
+    public void uploadJson() {
+    }
+
+    @FXML
+    public void onAdd() {
+    }
+
+    @FXML
+    public void onEdit() {
+    }
+
+    @FXML
+    public void onRemove() {
     }
 
 }
